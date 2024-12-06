71959b83b47bb4aec4736a5422f2a2571fa991b0
Joey Territo <joeyterrito@hotmail.com>
2024-12-05 22:47:04 -0600

    Add Nix build target

--- flake.nix
+++ flake.nix
@@ -4,6 +4,15 @@
   inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
   outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default =
+      let pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in with pkgs;
+      stdenv.mkDerivation {
+        name = "aoc2024";
+        src = self;
+        nativeBuildInputs = [ cmake catch2 ];
+      };
+
     devShells.x86_64-linux.default =
       let pkgs = nixpkgs.legacyPackages.x86_64-linux;
       in with pkgs;
